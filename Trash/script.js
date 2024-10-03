56e964724832b8ecc678acbd54da3d072a53ad0c
Zebooki <velogarciadiverge@gmail.com>
2024-10-03 14:49:18 +0200

    yeees

--- Trash/script.js
+++ Trash/script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("chatbox").hidden = true;
     document.getElementById("userChat").hidden = true;
+
+
+
     const inputElement = document.getElementById('input');
     const buttonElement = document.getElementById('btn');
+
   
     buttonElement.addEventListener('click', function() {
         const inputValue = inputElement.value;
@@ -11,22 +15,20 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('cont').hidden = true;
         document.getElementById('chatbox').hidden = false;
         document.getElementById("userChat").hidden = false;
-        user1 = "unset"
-        if(inputValue != ""){
-            user1 = inputValue;
-        }else{
-            user1 = "bum"
-        }
-        if(user1.value != "unset" && inputValue!= ""){
-            user2 = inputValue;
-        }else{
-            user2 = "bum2"
-        }
-
+        user1 = "User 1"
+        user2 = "User 2"
 
         document.getElementById('user1').innerHTML = user1;
         document.getElementById('user2').innerHTML = user2;
-
         
+    });
+
+    const sendButton = document.getElementById("send");
+    sendButton.addEventListener("click", function(){
+        const inputVal = document.getElementById("chat").value;
+        const message = document.createElement("div");
+        message.class = "message";
+        message.innerHTML = inputVal;
+        document.getElementById("userChat").appendChild(message); 
     });
   });
