556931edea8f6e8d81ddbe344328388e43944871
Zebooki <velogarciadiverge@gmail.com>
2024-11-14 23:37:52 +0100

    Vue Pushg

---
+++ main.vue
@@ -0,0 +1,20 @@
+<script>
+// JavaScript/Logic
+
+</script>
+
+<template>
+    <!-- Output Render -->
+    <div>
+        <h2>
+            Hello World! (in vue)
+        </h2>
+    </div>
+</template>
+
+<style>
+    /* CSS Styling */
+    body{
+        background-color: gray;
+    }
+</style>
