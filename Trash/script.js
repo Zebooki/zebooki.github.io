2296db5250aea8b41c1165818fd0c65cc0c4d74b
Zebooki <velogarciadiverge@gmail.com>
2024-11-14 18:41:24 +0100

    hehe

--- Trash/script.js
+++ Trash/script.js
@@ -1,34 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("chatbox").hidden = true;
-    document.getElementById("userChat").hidden = true;
 
+    document.getElementById("frame").hidden = true;
+    const login = document.getElementById("login");
+    const password = document.getElementById("password");
+    const container = document.getElementById("chatbox");
 
+    login.addEventListener("click", function() {
+        if(password.value == "as124151azsf1541"){
+            document.getElementById("frame").hidden = false;
+            container.hidden = true;
+        }else{
+            container.hidden = true;
+            const passwordWrong = document.createTextNode("nuh uh uh lil mf wrong password");
 
-    const inputElement = document.getElementById('input');
-    const buttonElement = document.getElementById('btn');
-
-  
-    buttonElement.addEventListener('click', function() {
-        const inputValue = inputElement.value;
-        console.log(inputValue);
-
-        document.getElementById('cont').hidden = true;
-        document.getElementById('chatbox').hidden = false;
-        document.getElementById("userChat").hidden = false;
-        user1 = "User 1"
-        user2 = "User 2"
-
-        document.getElementById('user1').innerHTML = user1;
-        document.getElementById('user2').innerHTML = user2;
-        
+            document.body.appendChild(passwordWrong);
+        }
     });
+    
 
-    const sendButton = document.getElementById("send");
-    sendButton.addEventListener("click", function(){
-        const inputVal = document.getElementById("chat").value;
-        const message = document.createElement("div");
-        message.class = "message";
-        message.innerHTML = inputVal;
-        document.getElementById("userChat").appendChild(message); 
-    });
-  });
+});
