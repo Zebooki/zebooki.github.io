2c398b2cc1c7b345e9246b7090517db7a916eb74
Zebooki <velogarciadiverge@gmail.com>
2024-11-15 12:14:13 +0100

    <3

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let text = "❤";
-console.log("Im In Love?");
-
-text += "❤";
-
-setTimeout(400);
-
-document.getElementById("love").innerHTML = text;
-
+window.addEventListener('DOMContentLoaded',function(){
+    console.log("Im In Love?");
+    this.setInterval(function(){
+        document.getElementById("love").style.color = "red";
+        this.setTimeout(function(){
+            document.getElementById("love").style.color = "#00ff00";
+        },325)
+    },750)
+    
+})
