a5817ca7b90e9f1ca9cb19f949e7529ce366d5ff
Zebooki <velogarciadiverge@gmail.com>
2024-11-15 01:13:33 +0100

    v

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let text = ""
-let inLove = true
+let text = "";
+console.log("Im In Love?");
+const inLove = true;
 while(inLove == true){
     text += "❤";
     setTimeout(function(){
         document.getElementById("love").innerHTML = text;
     },100);
     
-}
+};
