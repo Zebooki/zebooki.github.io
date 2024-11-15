7ad50696e4f3f14760b986521e2644720990a943
Zebooki <velogarciadiverge@gmail.com>
2024-11-15 01:19:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let text = "";
+let text = "❤";
 console.log("Im In Love?");
-let inLove = 0;
-while(inLove < 100){
-    text += "❤";
-    setTimeout(400);
-    document.getElementById("love").innerHTML = text;
-    inLove++;
-};
+
+text += "❤";
+
+setTimeout(400);
+
+document.getElementById("love").innerHTML = text;
+
