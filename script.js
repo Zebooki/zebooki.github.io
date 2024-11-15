43ce9be363748cd93c0e14a97805edcb5d224291
Zebooki <velogarciadiverge@gmail.com>
2024-11-15 22:37:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded',function(){
     console.log("Im In Love?");
     this.setInterval(function(){
-        document.getElementById("love").style.color = "red";
+        document.getElementById("love").style.color = "#8400ff";
         this.setTimeout(function(){
-            document.getElementById("love").style.color = "#00ff00";
+            document.getElementById("love").style.color = "#ff00d0";
         },325)
     },750)
     console.log("stop snooping around...")
