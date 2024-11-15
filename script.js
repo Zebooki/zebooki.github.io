2f4b7a3572eebdceeb85b971f491dcfb4929bc80
Zebooki <velogarciadiverge@gmail.com>
2024-11-15 01:17:49 +0100

    vvv

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 let text = "";
 console.log("Im In Love?");
 let inLove = 0;
-while(inLove < 10000){
+while(inLove < 100){
     text += "❤";
+    setTimeout(400);
+    document.getElementById("love").innerHTML = text;
     inLove++;
-    setTimeout(function(){
-        document.getElementById("love").innerHTML = text;
-    },100);
-    
 };
