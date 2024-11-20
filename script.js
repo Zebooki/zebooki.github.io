6bc9918964f95662db36c526e53e6210551aec83
Zebooki <velogarciadiverge@gmail.com>
2024-11-20 02:44:53 +0100

    hehe

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded',function () {
+    console.log('hi');
+
+    
+});
+
+function copyLink(){
+    // Copy the text inside the text field
+    navigator.clipboard.writeText('<a href="https://zebooki.neocities.org/"><img src="https://files.catbox.moe/cts28c.png" alt="Zebooki"></a>');
+
+}
+
